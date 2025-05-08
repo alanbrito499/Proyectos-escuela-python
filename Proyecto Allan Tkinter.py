deb2d3172894062414c29fdf9ec5bba4e410bbf1
alanbrito499 <allan.flores.cbtis03@gmail.com>
2025-05-07 22:24:21 -0600

    Version 3

--- Proyecto Allan Tkinter.py
+++ Proyecto Allan Tkinter.py
@@ -8,13 +8,14 @@ def saludar():
 ventana = tk.Tk()
 ventana.title("Mi primera app gráfica")
 ventana.geometry("400x200")
+ventana.config(bg="lightblue") 
 
-etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:")
+etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:", bg="lightgreen")
 etiqueta_nombre.pack()
 entrada_nombre = tk.Entry(ventana)
 entrada_nombre.pack()
 
-etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:")
+etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:", bg="lightgreen")
 etiqueta_edad.pack()
 entrada_edad = tk.Entry(ventana)
 entrada_edad.pack()
@@ -22,7 +23,10 @@ entrada_edad.pack()
 boton = tk.Button(ventana, text="Mostrar saludo", command=saludar)
 boton.pack()
 
-etiqueta_resultado = tk.Label(ventana, text="")
+etiqueta_resultado = tk.Label(ventana, text="", bg="lightblue")
 etiqueta_resultado.pack()
+
+etiqueta_autor = tk.Label(ventana, text="Autor: Allan Flores H", bg="lightgreen")
+etiqueta_autor.pack(side="bottom")
 
 ventana.mainloop()
